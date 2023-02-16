e5a7bdbef35311fd14f981b5991465983bbe86f5
gitocoder <surafelnigusie@gmail.com>
2023-02-16 15:26:16 -0800

    commit the 4 file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ *main-print "programing is like building a multiligual puzzel"
+ *
+ *return o
+ */
 int main(void)
 {
-	puts("\"programming is like building a multiligual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
