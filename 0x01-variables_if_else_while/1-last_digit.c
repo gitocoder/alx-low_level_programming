83c5f9d3e815f7c4de960943eb19742e4063a927
gitocoder <surafelnigusie@gmail.com>
2023-02-17 11:34:58 -0800

    upfdate the file for the last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,16 @@
+#include <stdlib.b>
+#include <time.h>
+/*more header goes there*/
+/*betty stylel doc for function main goes there*/
+int main(void)
+{
+	int n;
+	stand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/*your code goes there*/
+	m = n % 10 
+	if (m > 5)
+		printf("last digit of %i is %i and is grater than 5/n")
+
+	return (0);
+}
